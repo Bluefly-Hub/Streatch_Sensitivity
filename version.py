415bc5402aea6b08e8b174b0e35a88616f3d433d
Brad Smith <bsmith@ncsmultistage.com>
2025-11-12 15:19:16 -0600

    Change version number to 1.0.3

--- version.py
+++ version.py
@@ -3,5 +3,5 @@ Version information for Well Automation Application
 Update this version number when releasing new versions
 """
 
-VERSION = "1.0.2"  # Keep at 1.0.0 to test updating to newer release
+VERSION = "1.0.3"  # Keep at 1.0.0 to test updating to newer release
 __version__ = VERSION  # Also provide standard __version__ attribute
